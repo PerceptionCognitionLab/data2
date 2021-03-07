f13975972909723f01f1ed75d146f2c61896c0c6
root <root@specl.ss2k.uci.edu>
2021-03-07 13:18:08 -0800

    automatic update

---
+++ flank/dev/f1.py
@@ -0,0 +1,162 @@
+from psychopy import core, visual, sound, event
+import random
+import decimal
+import sys
+import numpy as np
+from PIL import Image   
+import os
+
+
+numTarg = 6
+numFlank = 2
+flankVal=[0,1]
+targVal=[0,.2,.4,.6,.8,1]
+
+def decode(cond):
+    return(divmod(cond,numFlank))
+
+def code(targ,flank):
+    return(targ*numFlank+flank)
+
+condVals=np.array(range(numTarg*numFlank),dtype=int)
+condRep=np.array([20,20,10,10,10,10,10,10,10,10,20,20],dtype=int)
+cond=np.repeat(condVals,condRep)
+np.random.shuffle(cond)
+
+resX=100
+resY=110
+
+sepX=resX+10
+sepY=resY+10
+posX=sepX*np.array((-1,0,1))
+posY=sepY*np.array((-1,0,1))
+
+##########################
+# SET UP THE EXPERIMENT ##
+##########################
+SCRIPT_DIR=os.environ.get('SCRIPT_DIR')
+sys.path.append(SCRIPT_DIR)
+from expLib import *
+
+useDB=True
+dbConf = exp
+expName='brFlank-p1'
+
+createTableStatement = (
+    "CREATE TABLE `out__" + expName + "` ("
+    "  `datID` INT(4) UNSIGNED NOT NULL AUTO_INCREMENT,"
+    "  `sessionID` INT(4) UNSIGNED NOT NULL,"
+    "  `block` INT(2) UNSIGNED NOT NULL,"
+    "  `trial` INT(2) UNSIGNED NOT NULL,"
+    "  `flanker` INT(2) UNSIGNED NOT NULL,"
+    "  `target` INT(2) UNSIGNED NOT NULL,"
+    "  `resp` INT(1) NOT NULL,"
+    "  `rt`  DECIMAL(5,3),"
+    "  PRIMARY KEY (`datID`)"
+    ") ENGINE=InnoDB")
+
+insertTableStatement = (
+     "INSERT INTO `out__" + expName + "` ("
+     "`sessionID`, `block`, `trial`, `flanker`, `target`, `resp`,`rt`)"
+     "VALUES (%s, %s, %s, %s, %s, %s, %s)")
+
+if useDB: 
+	sessionID=startExp(expName,createTableStatement,dbConf)
+else:
+	sessionID=1
+    
+########################################
+# GLOBAL SETTINGS  #####################
+########################################
+window=visual.Window(units= "pix", 
+                     allowGUI=False,
+                     size=(400,400),
+                     color=[0,0,0],
+                     fullscr = False)
+mouse = event.Mouse(visible=False)
+timer = core.Clock()
+seed = random.randrange(1e6)
+rng = random.Random(seed)
+
+correct1=sound.Sound(500,secs=.1)
+correct2=sound.Sound(1000,secs=.1)
+error=sound.Sound(300,secs=.2)
+####################################
+##  TRIAL SET UP   #################
+####################################
+
+    
+
+
+def makeArray(p,mx,my):
+    n=mx*my
+    x=int((1-p)*n+.5)
+    a0=np.zeros(x,dtype=np.uint8)
+    a1=np.ones(n-x,dtype=np.uint8)
+    a2=np.concatenate((a0,255*a1))
+    np.random.shuffle(a2)
+    a3=np.reshape(a2,(my,mx))
+    return(Image.fromarray(a3))
+
+
+def doTrial(cond):
+    blank=visual.TextStim(win=window,text="")
+    
+    [targ,flank]=decode(cond)
+    im=[]
+    for x in range(3):
+        for y in range(3):
+            t=makeArray(flankVal[flank],resX,resY)
+            im.append(visual.ImageStim(win=window,
+                               pos=(posX[x],posY[y]),
+                               image=t))
+    im[4].image=makeArray(targVal[targ],resX,resY)        
+    full=visual.BufferImageStim(win=window,stim=im)
+
+    correct=int(targVal[targ]>.5)
+    
+    blank.draw()
+    window.flip()
+    core.wait(.5)        
+    timer.reset()
+    full.draw()
+    window.flip()
+    keys=event.waitKeys(keyList=['z','slash','9'],timeStamped=timer)
+    blank.draw()
+    window.flip()
+    resp=keys[0][0]
+    rt=keys[0][1]
+    if resp=='9':
+        window.close()
+        core.quit()
+        exit()
+    if resp=='z':
+        respInt=0
+    else:
+        respInt=1
+    if respInt==correct:
+        correct1.play()
+        core.wait(0.1)
+        correct2.play()
+    else:
+        error.play()
+    core.wait(.5)
+    return(respInt,rt)
+
+#######################################
+### START EXPERIMENT ##################
+#######################################
+
+numTrials=len(cond)
+for n in range(numTrials):
+    (respInt,RT)=doTrial(cond[n])
+    rt = decimal.Decimal(RT).quantize(decimal.Decimal('1e-3'))
+    (targ,flank)=decode(cond[n])
+    addData = (sessionID,0,n,targ,flank,respInt,rt)
+    if useDB:
+        insertDatTable(insertTableStatement,addData,dbConf)
+    else:
+        print(addData)	
+window.close()
+core.quit()
+
---
+++ ssvep/efRsvp1/efRsvp1.py
@@ -0,0 +1,241 @@
+from psychopy import core, visual, sound, event
+import random
+import decimal
+import sys
+import numpy as np
+from PIL import Image   
+import os
+
+##########################
+# SET UP THE EXPERIMENT ##
+##########################
+SCRIPT_DIR=os.environ.get('SCRIPT_DIR')
+sys.path.append(SCRIPT_DIR)
+from expLib import *
+
+useDB=True
+dbConf = exp
+expName='efRsvp1'
+
+createTableStatement = (
+    "CREATE TABLE `out__" + expName + "` ("
+    "  `datID` INT(4) UNSIGNED NOT NULL AUTO_INCREMENT,"
+    "  `sessionID` INT(4) UNSIGNED NOT NULL,"
+    "  `block` INT(5) UNSIGNED NOT NULL,"
+    "  `trial` INT(10) UNSIGNED NOT NULL,"
+    "  `flanker` INT(2) UNSIGNED NOT NULL,"
+    "  `target` INT(2) UNSIGNED NOT NULL,"
+    "  `resp` INT(1) NOT NULL,"
+    "  `crit`  INT(1) UNSIGNED NOT NULL,"
+    "  `rt`  DECIMAL(5,3),"    
+    "  PRIMARY KEY (`datID`)"
+    ") ENGINE=InnoDB")
+
+insertTableStatement = (
+     "INSERT INTO `out__" + expName + "` ("
+     "`sessionID`, `block`, `trial`, `flanker`, `target`, `resp`, `crit`,`rt`)"
+     "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+
+if useDB: 
+	sessionID=startExp(expName,createTableStatement,dbConf)
+else:
+	sessionID=1
+    
+########################################
+# GLOBAL SETTINGS  #####################
+########################################
+window=visual.Window(units= "pix", 
+                     allowGUI=False,
+                     size=(600,120),
+                     color=[-1,-1,-1],
+                     fullscr = True)
+mouse = event.Mouse(visible=False)
+timer = core.Clock()
+seed = random.randrange(1e6)
+rng = random.Random(seed)
+
+
+########################################
+# CONDITIONS AND BLOCKS  ###############
+########################################
+nBlocks=4
+nTrials=20
+Rep=3
+cond=np.repeat(range(4),Rep)
+N=len(cond)
+order=random.sample(range(N),N)
+crit=np.random.randint(2,6,N)
+
+
+###############################
+# START UP   ##################
+###############################
+refreshRate=60
+numPlaces=5 
+numSlots=8
+picRes=[100,110]
+framesPerSlot =30
+numFrames=numSlots*framesPerSlot
+fBlank1 = 6
+fBlank2 = 30
+fFix = 30
+preRSVP = fBlank1+fBlank2+fFix
+
+setDir = "/home/exp/specl-exp/data2/ssvep/dev/set1"
+setPre = "set1-"
+setPost = ".png"
+freq=[3,3,2,3,3]
+center=numPlaces//2
+posX=[(a-center)*picRes[0] for a in range(numPlaces)]
+
+keysound=sound.Sound(225,secs=.07)
+correct1=sound.Sound(400,secs=.1)
+correct2=sound.Sound(600,secs=.2)
+error=sound.Sound(50,secs=.4)
+quitting=sound.Sound(70,secs=.2)
+
+
+abortKey='9'
+################################
+# CREATE IMAGES  ###############
+################################
+digImg=[]
+fname=[]
+for i in range(10):
+    fname.append(setDir+'/'+setPre+str(i)+setPost)
+    digImg.append(Image.open(fname[i]))
+
+digImg.append(Image.open(setDir+'/'+setPre+'AH-0.00'+setPost))
+digImg.append(Image.open(setDir+'/'+setPre+'AH-1.00'+setPost))
+digImg.append(Image.open(setDir+'/'+setPre+'+'+setPost))
+
+
+Xfix=[]
+for place in range(numPlaces):
+    Xfix.append(visual.ImageStim(win=window,
+                               pos=(posX[place],0),
+                               image=digImg[12],
+                               opacity=1.0))
+fix = visual.BufferImageStim(win=window,stim=Xfix,rect=[-.3,.1,.3,-.1])
+
+
+Blank=visual.TextStim(window, text = "", pos = (0,0))
+Welcome=visual.TextStim(window, text = "Identify the letter that appears at the center of the screen\n\n\n Press the 'q' key for A\n\n Press the 'p' key for H\n\n\n\n Press any key to begin", pos = (0,0))
+Pause=visual.TextStim(window, text = "Press any key to continue to the next block of trials", pos = (0,0))
+Reminder=visual.TextStim(window, text = "Please enter your response", pos = (0,0))
+Quit=visual.TextStim(window, text = "Quiting experiment...", pos = (0,0))
+Thnx=visual.TextStim(window, text = "Thank you for your time!\n\n You may call the experimenter now!", pos = (0,0))
+
+####################################
+##  TRIAL SET UP   #################
+####################################
+def doTrial(targ,flank,crit):
+
+    digits=[]
+    a=list(range(10))
+    for i in range(numPlaces):
+        random.shuffle(a)
+        digits.append(a[-numSlots:])
+
+    for place in range(numPlaces):
+        digits[place][crit]=10+flank
+    digits[center][crit]=10+targ
+
+    im=[]
+    for frame in range(numFrames):
+        [slot,local]=divmod(frame,framesPerSlot)
+        if local==0:
+            piece=[]
+            for place in range(numPlaces):
+                piece.append(visual.ImageStim(win=window,
+                               pos=(posX[place],0),
+                               image=digImg[digits[place][slot]],
+                               opacity=(frame%freq[place])/(freq[place]-1)))
+        else:
+            for place in range(numPlaces):
+                piece[place].opacity=(frame%freq[place])/(freq[place]-1)       
+        im.append(visual.BufferImageStim(win=window,stim=piece,rect=[-.3,.1,.3,-.1]))
+
+    for a in range(preRSVP):
+        if(a//fBlank1==0):
+            Blank.draw()            
+            window.flip()
+        elif(a//(fBlank1+fFix)==0):
+            fix.draw()
+            window.flip()
+        else:
+            Blank.draw()
+            window.flip()
+
+    myTimesB=[]
+    myTimesA=[]
+    timer.reset()
+    for frame in range(numFrames):
+        myTimesA.append(timer.getTime())    
+        im[frame].draw()
+        myTimesB.append(timer.getTime())
+        window.flip()
+    for frame in [0,1]:
+        Blank.draw()
+        window.flip()
+    keys=event.getKeys(keyList=['q','p',abortKey],timeStamped=timer)
+    if len(keys)==0:
+        Reminder.draw()
+        window.flip()       
+        keys=event.waitKeys(keyList=['q','p',abortKey],timeStamped=timer)
+    resp=keys[0][0]
+    rt=keys[0][1]
+    if resp==abortKey: 
+        for i in range(framesPerSlot):
+            Quit.draw()
+            window.flip()
+        exit()
+    if resp=='q':
+        respInt=0
+    else:
+        respInt=1  
+    if (respInt==targ):
+        correct1.play()
+        core.wait(0.1)
+        correct2.play()
+    else: 
+        error.play()
+    frameSkipped=0
+    for a in range(numFrames):
+        if myTimesB[a] - myTimesA[a]>(1/refreshRate-.001):
+            frameSkipped+=1
+    del(im)        
+       
+    core.wait(.5)
+    return(respInt,rt,frameSkipped)
+
+
+#######################################
+### START EXPERIMENT ##################
+#######################################
+Welcome.draw()
+window.flip()
+event.waitKeys()
+
+for block in range(nBlocks):
+    BlockID = block
+    for trial in range(nTrials):
+        (target,flanker)=divmod(cond[order[trial]],2)
+        [resp,RT,frameFlag]=doTrial(target,flanker,crit[trial])
+        rt = decimal.Decimal(RT).quantize(decimal.Decimal('1e-3'))
+        addData = (sessionID, BlockID, trial,flanker,target,resp,crit[trial],rt)        
+        if useDB:
+            insertDatTable(insertTableStatement,addData,dbConf)
+        else:
+            print(addData)	
+    Pause.draw()
+    window.flip()
+    event.waitKeys()
+
+
+Thnx.draw()
+window.flip()
+event.waitKeys()
+
+window.close()
+core.quit()
