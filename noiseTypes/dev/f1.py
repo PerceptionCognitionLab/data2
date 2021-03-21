3a37256208a5ad1f5e0d3c6b7572bd8b3da4b6ac
SPECL EXP ACCT <exp.pcl.mu@gmail.com>
2021-03-20 22:00:52 -0700

    automatic update

--- noiseTypes/dev/f1.py
+++ noiseTypes/dev/f1.py
@@ -7,10 +7,6 @@ from PIL import Image
 import os
 
 
-resX=200
-resY=200
-
-
 
 ##########################
 # SET UP THE EXPERIMENT ##
@@ -67,31 +63,77 @@ error=sound.Sound(300,secs=.2)
 ####################################
 
     
-
-
-
-def doTrial():
-
-    blank=visual.TextStim(win=window,text="")
-    grate= visual.GratingStim(win=window,sf=.05,size=300,mask="gauss",
+blank=visual.TextStim(win=window,text="")
+grate= visual.GratingStim(win=window,sf=.05,size=256,mask="gauss",
                               ori=1,opacity=.3)
-    noise=visual.NoiseStim(win=window,size=(300,300),noiseType="uniform",
+noise=visual.NoiseStim(win=window,size=(256,256),noiseType="uniform",
                            noiseElementSize=2,opacity=0)
+
+
+
+
+def doTrial(grateOri,noiseOp):
+    if grateOri<0:
+        side=0
+    else:
+        side=1
+    noise.opacity=noiseOp
+    grate.ori=grateOri
     blank.draw()
     window.flip()
     core.wait(.2)
     grate.draw()
     noise.draw()
     window.flip()
-    core.wait(3)
+    timer.reset()
+    keys=event.waitKeys(keyList=['z','slash','9'],timeStamped=timer)
+    resp=keys[0][0]
+    rt=keys[0][1]
+    if resp=='9':
+        window.close()
+        core.quit()
+        exit()
+    if resp=='z':
+        respInt=0
+    else:
+        respInt=1
+    if respInt==side:
+        correct=1
+        correct1.play()
+        core.wait(0.1)
+        correct2.play()
+    else:
+        correct=0
+        error.play()
+    core.wait(.5)
+    return(respInt,rt,correct)
     return()
 
 #######################################
 ### START EXPERIMENT ##################
 #######################################
 
-doTrial()
+# staircase noise
 
+side=[-1,1]*100
+np.random.shuffle(side)
+
+op=.7
+last=0
+for n in range(50):
+    [resp,rt,correct]=doTrial(5*side[n],op)
+    if correct==0:
+        op=op-.01
+        last=0
+    elif last==1:
+        op=op+.01
+        last=0
+    else: last=1
+    print(op,last,correct)
+        
+    
+    
+del blank;
 window.close()
 core.quit()
 
