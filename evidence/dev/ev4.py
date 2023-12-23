df8e7bc9c8d6e0c09182fc029ade9eeb3fb38dec
SPECL EXP ACCT <exp.pcl.mu@gmail.com>
2023-12-22 22:00:15 -0800

    automatic update

--- evidence/dev/ev4.py
+++ evidence/dev/ev4.py
@@ -26,21 +26,27 @@ dot = visual.Circle(win,radius=5,units="pix",fillColor=[1,1,1])
 
 start = visual.TextStim(
 	win,
-	text="Press Any Key to Start",
+	text="In this experiment you can only press either X or M. Press Any Key to Start",
 	height=20,
 	color='white'
 )
-
 
 
 start.draw()
 win.flip()
 event.waitKeys()
 
-divider = visual.Line(
+# 384, -384
+dividerLow = visual.Line(
 	win=win,
-	start=(0,-384),
-	end=(0,384),
+	start=(0,-584),
+	end=(0,-400),
+	lineColor=[1,0,0]
+)
+dividerHigh = visual.Line(
+	win=win,
+	start=(0,400),
+	end=(0,584),
 	lineColor=[1,0,0]
 )
 
@@ -53,30 +59,36 @@ def doTrial (mean):
 	x0=np.rint(np.random.normal(mean,100,size=maxE))
 	x=np.repeat(x0,frameDur)
 	resp=[]
+	fix.draw()
+	win.flip()
+	core.wait(1)
 	while (currentFrame<(maxE*frameDur) and len(resp)==0):		
-		divider.draw()
+		dividerLow.draw()
+		dividerHigh.draw()
 		dot.pos=[x[currentFrame],0]
 		dot.draw()
 		win.flip()
 		currentFrame=currentFrame+1
-		resp=event.getKeys()
+		resp=event.getKeys(keyList=['x','m','escape'])
 		if 'escape' in resp:
+			fptr.close()
+			win.close()
 			return None, 'Abort', None
-	return(currentFrame,resp[0],x0)
+		elif resp:
+			break
+	return currentFrame,resp[0] if resp else None, x0
 
 
 
-initial_condition = -50
-num_trials = 4
-for i in range(num_trials):
-	[rt, resp, x] = doTrial(initial_condition)
-	if resp == 'Abort':
-		print("Experiment Aborted")
-		break
-	print(sub, rt, resp, *x, sep=", ", file=fptr)
-	fptr.flush()
-	initial_condition *= -1
-
+conditions = [-50,-50,-50,-50,-50,50,50,50,50,50]
+random.shuffle(conditions)
+for condition in conditions:
+    [rt, resp, x] = doTrial(condition)
+    if resp == 'Abort':
+        print("Experiment Aborted")
+        break
+    print(sub, rt, resp, *x, sep=", ", file=fptr)
+    fptr.flush()
 
 fptr.close()
 win.close()
